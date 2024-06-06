602c42f4040f167cebd6826176c46316f7633043
Oseias Lopes <Oseias.YC@gmail.com>
2024-06-06 14:37:27 -0300

    getTableCode() finished

--- src/main/java/com/yc/Main.java
+++ src/main/java/com/yc/Main.java
@@ -1,23 +1,39 @@
 package com.yc;
 
 import java.io.StringReader;
+import java.lang.reflect.Field;
 
 import com.yc.parser.ParseException;
 import com.yc.parser.Parser;
+import com.yc.parser.ParserConstants;
 import com.yc.parser.Token;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, NoSuchFieldException {
         String codigo = "cadeia () \n9";
 
-        Parser lexer = new Parser(new StringReader(codigo));
+        Parser parser = new Parser(new StringReader(codigo));
 
-        Token token = lexer.getNextToken();
+        Token token = parser.getNextToken();
 
         System.out.println("Código analisado: " + codigo + "\n");
         while (token.kind != 0) {
-            System.out.println("Código: " + token.kind + " | Lexema: " + token.image + " | Linha: " + token.beginLine);
-            token = lexer.getNextToken();
+            System.out.println("Código: " + token.kind + " | Lexema: " + token.image + " | Linha: " + token.beginLine
+                    + " | Código na Tabela: " + getTableCode(token.kind));
+            token = parser.getNextToken();
         }
+    }
+
+    public static String getTableCode(int kind) throws NoSuchFieldException, SecurityException {
+        Field[] fields = ParserConstants.class.getDeclaredFields();
+
+        for (Field field : fields) {
+            try {
+                if (field.getInt(null) == kind) {
+                    return field.getName();
+                }
+            } catch (IllegalAccessException e){}
+        }
+        throw new NoSuchFieldException("Código da Tabela não encontrado.");
     }
 }
