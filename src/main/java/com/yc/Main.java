e42675900c81bfb1971946247e32c060e58f5c21
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2024-06-05 21:33:55 -0300

    Added Parser

--- src/main/java/com/yc/Main.java
+++ src/main/java/com/yc/Main.java
@@ -1,6 +1,23 @@
 package com.yc;
 
+import java.io.StringReader;
+
+import com.yc.parser.ParseException;
+import com.yc.parser.Parser;
+import com.yc.parser.Token;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        String codigo = "cadeia () \n9";
+
+        Parser lexer = new Parser(new StringReader(codigo));
+
+        Token token = lexer.getNextToken();
+
+        System.out.println("Código analisado: " + codigo + "\n");
+        while (token.kind != 0) {
+            System.out.println("Código: " + token.kind + " | Lexema: " + token.image + " | Linha: " + token.beginLine);
+            token = lexer.getNextToken();
+        }
     }
 }
