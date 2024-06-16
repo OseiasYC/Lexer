07c278b48569c75168ea5fa19771f417b5394d6a
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2024-06-16 16:21:46 -0300

    Unclosed comment fixes

--- src/main/java/com/yc/parser/Parser.java
+++ src/main/java/com/yc/parser/Parser.java
@@ -321,15 +321,20 @@ public class Parser implements ParserConstants {
   final private int[] jj_la1 = new int[2];
   static private int[] jj_la1_0;
   static private int[] jj_la1_1;
+  static private int[] jj_la1_2;
   static {
 	   jj_la1_init_0();
 	   jj_la1_init_1();
+	   jj_la1_init_2();
 	}
 	private static void jj_la1_init_0() {
-	   jj_la1_0 = new int[] {0xffffff80,0xffffff80,};
+	   jj_la1_0 = new int[] {0xffffff00,0xffffff00,};
 	}
 	private static void jj_la1_init_1() {
-	   jj_la1_1 = new int[] {0x7fffffff,0x7fffffff,};
+	   jj_la1_1 = new int[] {0xffffffff,0xffffffff,};
+	}
+	private static void jj_la1_init_2() {
+	   jj_la1_2 = new int[] {0x0,0x0,};
 	}
 
   /** Constructor with InputStream. */
@@ -454,7 +459,7 @@ public class Parser implements ParserConstants {
   /** Generate ParseException. */
   public ParseException generateParseException() {
 	 jj_expentries.clear();
-	 boolean[] la1tokens = new boolean[64];
+	 boolean[] la1tokens = new boolean[65];
 	 if (jj_kind >= 0) {
 	   la1tokens[jj_kind] = true;
 	   jj_kind = -1;
@@ -468,10 +473,13 @@ public class Parser implements ParserConstants {
 		   if ((jj_la1_1[i] & (1<<j)) != 0) {
 			 la1tokens[32+j] = true;
 		   }
+		   if ((jj_la1_2[i] & (1<<j)) != 0) {
+			 la1tokens[64+j] = true;
+		   }
 		 }
 	   }
 	 }
-	 for (int i = 0; i < 64; i++) {
+	 for (int i = 0; i < 65; i++) {
 	   if (la1tokens[i]) {
 		 jj_expentry = new int[1];
 		 jj_expentry[0] = i;
